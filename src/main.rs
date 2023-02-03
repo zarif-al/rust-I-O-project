6361648745a67467ba37129bff5337e32180e6cb
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-03 16:17:14 +0000

    Update code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use std::env;
-
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
 
-    let query = &args[1];
-    let file = &args[2];
-    println!("Looking for: {}", query);
-    println!("In file: {}", file);
+    let contents = fs
+        ::read_to_string(config.file_path)
+        .expect("Should have been able to read a file");
+
+    println!("File Contents:\n{contents}")
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
