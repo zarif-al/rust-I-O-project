55cbd7b432253a048b220bb91ab6b6063f39e936
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-04 10:28:32 +0000

    Implement clean exit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::env;
-use std::fs;
+use std::{ env, fs, process };
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
     let contents = fs
         ::read_to_string(config.file_path)
@@ -17,10 +20,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("There are not enough arguments.");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
