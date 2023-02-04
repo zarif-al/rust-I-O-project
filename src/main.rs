7537a8392181c44909da1dcc941c6d6661fb8d89
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-02-04 10:48:44 +0000

    Separate code into library

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{ env, fs, process };
+use std::{ env, process };
+
+use workspace::{ Config, run };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,26 +9,8 @@ fn main() {
         process::exit(1)
     });
 
-    let contents = fs
-        ::read_to_string(config.file_path)
-        .expect("Should have been able to read a file");
-
-    println!("File Contents:\n{contents}")
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("There are not enough arguments.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1)
     }
 }
